193524187047b1c292d674519f102c45f5efdc9e
Leonardo Souza Dias <leonnardosouza96@gmail.com>
2020-12-30 16:52:27 -0300

    desafio finalizado

---
+++ README.md
@@ -0,0 +1,8 @@
+### Desafio TypeORM Relations
+
+Esse projeto se aplica ao desafio 09 da turma 13 do Bootcamp GoStack da Rocketseat.
+
+### O desafio
+
+Trata-se de uma api em Node.js para realização de cadastro de clientes, e produtos, e realizar venda destes, com relacionamentos entre as entidades.
+
--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,72 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
-  ) {}
+  ) { }
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customerExists = await this.customersRepository.findById(customer_id);
+
+    if (!customerExists) {
+      throw new AppError('Não encontrado cliente com esse id.');
+    }
+
+    const existentProducts = await this.productsRepository.findAllById(products);
+
+    if (!existentProducts.length) {
+      throw new AppError('Não encontrado produtos com esse id.');
+    }
+
+    const existentProductsIds = existentProducts.map(product => product.id);
+
+    const checkInexistentsProducts = products.filter(
+      product => !existentProductsIds.includes(product.id),
+    );
+
+    if (checkInexistentsProducts.length) {
+      throw new AppError(`Produto não encontrado: ${checkInexistentsProducts[0].id}`)
+    }
+
+    const findProductsWithNoQuantityAvailable = products.filter(
+      product =>
+        existentProducts.filter(p => p.id === product.id)[0].quantity < product.quantity,
+    );
+
+    if (findProductsWithNoQuantityAvailable.length) {
+      throw new AppError(
+        `A quantidade ${findProductsWithNoQuantityAvailable[0].quantity} não está disponível para o produto ${findProductsWithNoQuantityAvailable[0].id}`
+      );
+    }
+
+    const serializedProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: existentProducts.filter(p => p.id === product.id)[0].price,
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer: customerExists,
+      products: serializedProducts,
+    });
+
+    const { order_products } = order;
+
+    const orderedProductsQuantity = products.map(product => ({
+      id: product.id,
+      quantity:
+        existentProducts.filter(p => p.id === product.id)[0].quantity - product.quantity,
+    }));
+
+    await this.productsRepository.updateQuantity(orderedProductsQuantity);
+
+    return order;
   }
 }
 
